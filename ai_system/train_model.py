abdf38e79506f7b75466d71c88111a615d447683
dzkiran07 <kasalawotkiran2@gmail.com>
2026-08-13 19:57:30 +0545

    Update training model to include death and advancement counts, and increase max depth

--- ai_system/train_model.py
+++ ai_system/train_model.py
@@ -6,12 +6,12 @@ import pickle
 
 data = pd.read_csv("training_data.csv")
 
-X = data[["action_count", "idle_time", "session_length"]]
+X = data[["action_count", "idle_time", "session_length", "death_count", "advancement_count"]]
 y = data["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = DecisionTreeClassifier(max_depth=4, random_state=42)
+model = DecisionTreeClassifier(max_depth=6, random_state=42)
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
